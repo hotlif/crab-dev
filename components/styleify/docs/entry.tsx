de8932f5c68f55aeed5a59cff6eae4c85a3c8a85
hotlif <zhangjin0908@hotmail.com>
2025-01-03 10:00:13 +0800

    feat: 添加 react-router 作为文档的基础路由

--- components/styleify/docs/entry.tsx
+++ components/styleify/docs/entry.tsx
@@ -1,9 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router";
+import Layout from "./layouts"
+
+const router = createBrowserRouter([{
+	path: "/",
+	element: <Layout />,
+	children: [
+	]
+}]);
 
 const App = () => {
     return (
-        <>
-        </>
+        <RouterProvider
+			router={router}
+		/>
     )
 }
 
