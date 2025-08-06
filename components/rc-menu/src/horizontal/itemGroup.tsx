a0fa7cf840b9a2df5952d30c54389a605cdff672
hotlif <zhangjin0908@hotmail.com>
2025-08-06 10:48:48 +0800

    fix: 优化项目结构

---
+++ components/rc-menu/src/horizontal/itemGroup.tsx
@@ -0,0 +1,55 @@
+import { cx } from "@linaria/core";
+import { type FC, type ReactNode } from "react";
+
+import { type Item } from "../type";
+import groupStyle from "./styles/itemGroup.styles";
+
+
+interface GroupItemProps {
+    item: Item,
+    children: ReactNode[],
+    depth: number
+}
+
+const GroupItem: FC<GroupItemProps> = ({
+    item,
+    children,
+    depth
+}) => {
+    return (
+        <li
+            key={item.key}
+            className={cx(groupStyle.groupItem.container)}
+        >
+            <div
+                className={cx(groupStyle.groupItem.header)}
+                style={{
+                    paddingLeft: `calc(${depth} * 5px * 0.7)`
+                }}
+            >
+                {
+                    item.icon && (
+                        <span
+                            className={groupStyle.groupItem.icon}
+                        >
+                            {item.icon}
+                        </span>
+                    )
+                }
+                <span
+                    className={groupStyle.groupItem.title}
+                >
+                    {item.title}
+                </span>
+            </div>
+            <ul
+                className={groupStyle.childrenList.container}
+            >
+                {children}
+            </ul>
+        </li>
+    )
+}
+
+
+export default GroupItem;
---
+++ components/rc-menu/src/horizontal/styles/itemGroup.styles.ts
@@ -0,0 +1,48 @@
+import { css } from "@linaria/core"
+
+const groupItemContainer = css`
+    font-size: 14px;
+    position: relative;
+    user-select: none;
+    transition: border-color 0.3s, color 0.3s;
+`
+
+const groupChildrenList = css`
+    list-style-type: none;
+    padding-inline-start: unset;
+    color: #000;
+    line-height: 40px;
+    border-radius: 4px;
+`
+
+const groupItemHeader = css`
+    cursor: default;
+    border-radius: 4px;
+    padding-inline: 16px;
+    display: flex;
+    align-items: center;
+`
+
+const groupItemTitle = css`
+    opacity: 0.5;
+`
+
+const groupItemIcon = css`
+    opacity: 0.5;
+    margin-right: 8px;
+`
+
+
+const styles = {
+    groupItem: {
+        container: groupItemContainer,
+        header: groupItemHeader,
+        icon: groupItemIcon,
+        title: groupItemTitle,
+    },
+    childrenList: {
+        container: groupChildrenList,
+    },
+};
+
+export default styles;
