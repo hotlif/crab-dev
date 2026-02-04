cd15440b45c4a39db7aee0676be5303a4260192f
hotlif <zhangjin0908@hotmail.com>
2026-02-04 09:36:00 +0800

    chore: 优化代码， 增加可读性

--- toolbox/crustify/src/presetWebpack/module.ts
+++ toolbox/crustify/src/presetWebpack/module.ts
@@ -6,90 +6,90 @@ import { type Configuration } from "webpack";
 
 const require = createRequire(import.meta.url);
 
+// 获取 remark 插件的默认导出
+function getRemarkPlugin<T>(plugin: T): T {
+    return (plugin as unknown as { default?: T }).default ?? plugin;
+}
+
+const getBabelLoader = () => ({
+    loader: require.resolve("babel-loader"),
+    options: {
+        presets: [
+            [require.resolve("@babel/preset-env"), {
+                // @wyw-in-js/webpack-loader 不支持 @babel/plugin-transform-template-literals 
+                exclude: ['@babel/plugin-transform-template-literals'],
+            }],
+            [require.resolve("@babel/preset-typescript"), {}],
+            [require.resolve("@babel/preset-react"), { runtime: "automatic" }]
+        ],
+        plugins: [
+            [require.resolve("babel-plugin-react-compiler"), { target: '19' }],
+            [require.resolve("@crab-dev/babel-plugin-auto-import-style")]
+        ]
+    },
+});
+
+const getRules = (isProduction: boolean) => [
+    {
+        test: /\.css$/i,
+        use: [
+            isProduction ? MiniExtractPlugin.loader : require.resolve("style-loader"),
+            require.resolve("css-loader"),
+            require.resolve("lightningcss-loader"),
+        ],
+    },
+    {
+        test: /\.raw$/i,
+        exclude: /node_modules/,
+        type: 'asset/source',
+    },
+    {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+            { loader: require.resolve('@wyw-in-js/webpack-loader') },
+            require.resolve("thread-loader"),
+            getBabelLoader()
+        ],
+    },
+    {
+        test: /\.mdx?$/,
+        exclude: /node_modules/,
+        use: [
+            { loader: require.resolve('@wyw-in-js/webpack-loader') },
+            require.resolve("thread-loader"),
+            getBabelLoader(),
+            {
+                loader: require.resolve('@mdx-js/loader'),
+                options: {
+                    format: "mdx",
+                    providerImportSource: require.resolve("@mdx-js/react"),
+                    remarkPlugins: [
+                        getRemarkPlugin(remarkGfm),
+                        [getRemarkPlugin(remarkFrontmatter), "toml"]
+                    ],
+                    rehypePlugins: []
+                }
+            }
+        ]
+    }
+];
+
 /**
  * 生成带有指定预设和插件的 webpack 模块配置。
- *
- * @param {Object} options - 选项对象。
- * @param {boolean} options.isProduction - 指示构建是否用于生产环境。
- * @returns {Configuration} webpack 模块配置。
  */
 const presetModule = async ({
     isProduction
 }: {
     isProduction: boolean
 }) => {
-    const remarkGfmPlugin = (remarkGfm as unknown as { default?: typeof remarkGfm }).default ?? remarkGfm;
-    const remarkFrontmatterPlugin = (remarkFrontmatter as unknown as { default?: typeof remarkFrontmatter }).default ?? remarkFrontmatter;
-    const babelLoader = {
-        loader: require.resolve("babel-loader"),
-        options: {
-            presets: [
-                [require.resolve("@babel/preset-env"), {
-                    // @wyw-in-js/webpack-loader 不支持 @babel/plugin-transform-template-literals 
-                    exclude: ['@babel/plugin-transform-template-literals'],
-                }],
-                [require.resolve("@babel/preset-typescript"), {
-                }],
-                [require.resolve("@babel/preset-react"), {
-                    runtime: "automatic"
-                }]
-            ],
-            plugins: [
-                [require.resolve("babel-plugin-react-compiler"), {
-                    target: '19'
-                }],
-                [require.resolve("@crab-dev/babel-plugin-auto-import-style")]
-            ]
-        },
-    };
-
     const moduleConfiguration: Configuration = {
         module: {
-            rules: [{
-                test: /\.css$/i,
-                use: [
-                    isProduction ? MiniExtractPlugin.loader : require.resolve("style-loader"),
-                    require.resolve("css-loader"),
-                    require.resolve("lightningcss-loader"),
-                ],
-            }, {
-                test: /\.raw$/i,
-                exclude: /node_modules/,
-                type: 'asset/source',
-            }, {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: require.resolve('@wyw-in-js/webpack-loader'),
-                    },
-                    require.resolve("thread-loader"),
-                    babelLoader
-                ],
-            }, {
-                test: /\.mdx?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: require.resolve('@wyw-in-js/webpack-loader'),
-                    },
-                    require.resolve("thread-loader"),
-                    babelLoader,
-                    {
-                        loader: require.resolve('@mdx-js/loader'),
-                        options: {
-                            format: "mdx",
-                            providerImportSource:  require.resolve("@mdx-js/react"),
-                            remarkPlugins: [remarkGfmPlugin, [remarkFrontmatterPlugin, "toml"]],
-                            rehypePlugins: []
-                        }
-                    }
-                ]
-            }]
+            rules: getRules(isProduction)
         }
-    }
+    };
     return moduleConfiguration;
-}
+};
 
 export default presetModule;
 
