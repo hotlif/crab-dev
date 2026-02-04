85aa195f094b624891f7793b36e44f2cc66851f9
hotlif <zhangjin0908@hotmail.com>
2026-02-04 17:15:11 +0800

    feat: 添加 外观设置的 demo

--- components/rc-button/docs/demos/size.demo.tsx
+++ components/rc-button/docs/demos/size.demo.tsx
@@ -6,25 +6,60 @@
 
 import Button from "@crab-dev/rc-button";
 import { css } from "@linaria/core";
+import { useState } from "react";
 
 const SizeDemo = () => {
+    const [size, setSize] = useState<"large" | "middle" | "small">("middle")
+
     return (
         <div
             className={css`
-                display: flex;
-                align-items: center;
-                gap: 1rem;
+                margin-bottom: 1rem;
             `}
         >
-            <Button size="small">
-                small
-            </Button>
-            <Button size="middle">
-                medium
-            </Button>
-            <Button size="large">
-                large
-            </Button>
+            <div
+                className={css`
+                    display: flex;
+                    align-items: center;
+                    gap: 0.5rem;
+                    margin-bottom: 2rem;
+                `}
+            >
+                <label>
+                    请选择大小
+                </label>
+                <select
+                    value={size}
+                    onChange={e => setSize(e.target.value as "large" | "middle" | "small")}
+                >
+                    <option value="large">Large</option>
+                    <option value="middle">Middle</option>
+                    <option value="small">Small</option>
+                </select>
+            </div>
+            <div
+                className={css`
+                    display: flex;
+                    align-items: center;
+                    gap: 1rem;
+                `}
+            >
+                <Button appearance="primary" size={size}>
+                    primary
+                </Button>
+                <Button appearance="subtle" size={size}>
+                    subtle
+                </Button>
+                <Button appearance="dashed" size={size}>
+                    dashed
+                </Button>
+                <Button appearance="text" size={size}>
+                    text
+                </Button>
+                <Button appearance="link" size={size}>
+                    link
+                </Button>
+            </div>
         </div>
     )
 }
