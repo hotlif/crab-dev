98f647cd8d717793fc0cb2203bb7b6a03bf5f935
hotlif <zhangjin0908@hotmail.com>
2026-04-02 13:45:21 +0800

    feat(rc-virtual): show scrollbar only on hover with fade transition

--- components/rc-virtual/src/style/grid.style.ts
+++ components/rc-virtual/src/style/grid.style.ts
@@ -1,7 +1,15 @@
 import { css } from "@linaria/core";
+import { containerStyle as scrollbarContainerStyle } from "./scrollbar.style";
 
 export const containerStyle = css`
 	position: relative;
+	& > .${scrollbarContainerStyle} {
+		opacity: 0;
+		transition: opacity 150ms;
+	}
+	&:hover > .${scrollbarContainerStyle} {
+		opacity: 1;
+	}
 `;
 
 export const gridStyle = css`
