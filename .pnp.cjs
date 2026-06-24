a3b081c2b1c11e6e5dcd9f5c5b76431efc57f881
hotlif <zhangjin0908@hotmail.com>
2026-06-24 15:40:26 +0800

    chore(rc-color-picker): add rc-masonry workspace dependency

--- .pnp.cjs
+++ .pnp.cjs
@@ -5160,6 +5160,7 @@ const RAW_RUNTIME_STATE =
           ["@crab-dev/rc-color-picker", "workspace:components/rc-color-picker"],\
           ["@crab-dev/rc-component-preview", "workspace:components/rc-component-preview"],\
           ["@crab-dev/rc-dropdown-container", "workspace:components/rc-dropdown-container"],\
+          ["@crab-dev/rc-masonry", "workspace:components/rc-masonry"],\
           ["@crab-dev/rc-menu", "virtual:06fa0e3761b88af997d10420ce93b180d5556f99c9dcd1c21a62e010243745186953ec9dbe94bcc2ff2ee1612ee23164d14f9e03bbdc3ff5502ef925acc3d0d8#workspace:components/rc-menu"],\
           ["@crab-dev/rc-slider", "workspace:components/rc-slider"],\
           ["@crab-dev/rc-token-semantic", "workspace:components/rc-token-semantic"],\
