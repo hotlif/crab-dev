6f12d20c1fc77277c3aaf3e125542285b4d093cd
hotlif <zhangjin0908@hotmail.com>
2026-06-28 18:00:43 +0800

    fix(crustify): use node preset transform to fix jest ESM compatibility

--- toolbox/crustify/jest.config.mjs
+++ toolbox/crustify/jest.config.mjs
@@ -1,16 +1,2 @@
 import { node } from "@crab-dev/standards-jest-preset";
-export default {
-    ...node,
-    transform: {
-        "\\.[jt]sx?$": [
-            "babel-jest",
-            {
-                presets: [
-                    ["@babel/preset-env", { targets: "defaults", modules: false }],
-                    "@babel/preset-typescript",
-                    ["@babel/preset-react", { runtime: "automatic" }],
-                ],
-            },
-        ],
-    },
-};
+export default node;
