6b6e87825b73e8e0487d0f791beddaffd5dfdc63
hotlif <zhangjin0908@hotmail.com>
2026-07-01 12:41:34 +0800

    refactor(rc-protocol-table): extract SVG icons into icons.tsx

---
+++ components/rc-protocol-table/src/icons.tsx
@@ -0,0 +1,80 @@
+export function ColumnsIcon() {
+    return (
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="currentColor" aria-hidden="true">
+            <path d="M1 2.5A.5.5 0 0 1 1.5 2h2a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-10zm4 0A.5.5 0 0 1 5.5 2h3a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-10zm5 0A.5.5 0 0 1 10.5 2h3a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-10z" />
+        </svg>
+    );
+}
+
+export function FiltersIcon() {
+    return (
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="currentColor" aria-hidden="true">
+            <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 .79 1.615L9.5 8.5V13a.5.5 0 0 1-.777.416l-3-2A.5.5 0 0 1 5.5 11V8.5L1.21 3.615A1 1 0 0 1 1 3V2z" />
+        </svg>
+    );
+}
+
+export function ExpandAllIcon() {
+    return (
+        <svg width="14" height="14" viewBox="0 0 15 15" fill="currentColor" aria-hidden="true">
+            <path d="M1 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1H2V10a.5.5 0 0 1-1 0V5.5zm8 0a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V6h-3.5a.5.5 0 0 1-.5-.5zM7.5 8a.5.5 0 0 1 .354.146l3 3a.5.5 0 0 1-.708.708L7.5 9.207l-2.646 2.647a.5.5 0 0 1-.708-.708l3-3A.5.5 0 0 1 7.5 8zm0-6a.5.5 0 0 1 .354.146l3 3a.5.5 0 0 1-.708.708L7.5 3.207 4.854 5.854a.5.5 0 1 1-.708-.708l3-3A.5.5 0 0 1 7.5 2z"/>
+        </svg>
+    );
+}
+
+export function CollapseAllIcon() {
+    return (
+        <svg width="14" height="14" viewBox="0 0 15 15" fill="currentColor" aria-hidden="true">
+            <path d="M1 9.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1H2V5a.5.5 0 0 0-1 0v4.5zm8 0a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 0-1 0V9h-3.5a.5.5 0 0 0-.5.5zM7.5 7a.5.5 0 0 0 .354-.146l3-3a.5.5 0 1 0-.708-.708L7.5 5.793 4.854 3.146a.5.5 0 1 0-.708.708l3 3A.5.5 0 0 0 7.5 7zm0 6a.5.5 0 0 0 .354-.146l3-3a.5.5 0 0 0-.708-.708L7.5 12.793l-2.646-2.647a.5.5 0 0 0-.708.708l3 3A.5.5 0 0 0 7.5 13z"/>
+        </svg>
+    );
+}
+
+export function ResetWidthIcon() {
+    return (
+        <svg width="13" height="13" viewBox="0 0 15 15" fill="currentColor" aria-hidden="true">
+            <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 0 1 0V2h3.5a.5.5 0 0 0 0-1H1.5zm9.5.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v3.5a.5.5 0 0 1-1 0V2H11a.5.5 0 0 1-.5-.5zM1 10.5a.5.5 0 0 1 .5-.5H5a.5.5 0 0 1 0 1H2v2.5a.5.5 0 0 1-1 0V10.5zm13 0a.5.5 0 0 1-.5.5H11v2.5a.5.5 0 0 1-1 0V10.5a.5.5 0 0 1 .5-.5h3.5a.5.5 0 0 1 .5.5z"/>
+        </svg>
+    );
+}
+
+export function RefreshIcon() {
+    return (
+        <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
+            <path fillRule="evenodd" d="M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2z"/>
+            <path d="M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466"/>
+        </svg>
+    );
+}
+
+export function ExportIcon() {
+    return (
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="currentColor" aria-hidden="true">
+            <path d="M7.5 1a.5.5 0 0 1 .5.5v7.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7 9.293V1.5a.5.5 0 0 1 .5-.5zm-5 11a.5.5 0 0 0 0 1h10a.5.5 0 0 0 0-1h-10z"/>
+        </svg>
+    );
+}
+
+export function SearchPrevIcon() {
+    return (
+        <svg width="13" height="13" viewBox="0 0 15 15" fill="currentColor" aria-hidden="true">
+            <path d="M7.5 11.5a.5.5 0 0 1-.354-.146l-4-4a.5.5 0 0 1 .708-.708L7.5 10.293l3.646-3.647a.5.5 0 0 1 .708.708l-4 4A.5.5 0 0 1 7.5 11.5z"/>
+        </svg>
+    );
+}
+
+export function SearchNextIcon() {
+    return (
+        <svg width="13" height="13" viewBox="0 0 15 15" fill="currentColor" aria-hidden="true">
+            <path d="M7.5 3.5a.5.5 0 0 0-.354.146l-4 4a.5.5 0 0 0 .708.708L7.5 4.707l3.646 3.647a.5.5 0 0 0 .708-.708l-4-4A.5.5 0 0 0 7.5 3.5z"/>
+        </svg>
+    );
+}
+
+export function ErrorIcon() {
+    return (
+        <svg width="24" height="24" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg>
+    );
+}
