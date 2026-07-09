820c78e20e96ef90de0babb03bb56481c19d9187
hotlif <zhangjin0908@hotmail.com>
2026-07-09 15:21:19 +0800

    fix(rc-form): export RuleType enum as a value not type-only

--- components/rc-form/src/index.ts
+++ components/rc-form/src/index.ts
@@ -2,7 +2,7 @@
 import Form, { type FormProps } from "./form.js";
 import Item, { type FormItem as ItemProps} from "./item.js";
 import useForm from "./hooks/useForm.js";
-import { type NamePath, ValidateState, type FormInstance, type FormItemEditor, type Rule, type RuleType } from "./types.js";
+import { type NamePath, ValidateState, type FormInstance, type FormItemEditor, type Rule, RuleType } from "./types.js";
 
 export {
     Item,
